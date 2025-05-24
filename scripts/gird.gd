a4eb8d9d953afa0fadb753176098b605b2c1c8d8
uspe8888 <uspe8888@gmail.com>
2025-05-24 19:30:16 +0800

    Revert "提交1"
    This reverts commit 6abc197eb8b6cf92e432576b49167503b6fc486f.

--- scripts/gird.gd
+++ scripts/gird.gd
@@ -28,7 +28,10 @@ var pieces={}
 var cols:int=9 # 列
 var rows:int=9 # 行
 
-#caocaocao
+
+#操操操操操操操操操操操操操
+#操操操操操操操操操操操操操
+#操操操操操操操操操操操操操
 
 # 预加载背景单元格场景
 var back_cell=preload("res://scenes/background.tscn")
